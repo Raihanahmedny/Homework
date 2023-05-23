db00a8a82e5165c3ce45e47d996482924157d46c
Raihanahmedny <Raihanahmedny@gmail.com>
2023-05-23 01:15:13 -0400

    homework

---
+++ src/hw5Q3Constructor/StudentTest.java
@@ -0,0 +1,9 @@
+package hw5Q3Constructor;
+
+public class StudentTest {
+
+	public static void main(String[] args) {
+		Student student = new Student();
+		Student student01 = new Student("Raihan Ahmed", 200933982, 'M', true, 99.99f);
+	}
+}
